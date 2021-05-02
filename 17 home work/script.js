194162bb7c5ad2e4b0bc8916aaf9397bc834a149
AlenaShishkina <79803218+AlenaShishkina@users.noreply.github.com>
2021-05-02 16:32:29 +0300

    Add files via upload

---
+++ 17 home work/script.js
@@ -0,0 +1,25 @@
+const url = "https://rickandmortyapi.com/api/character/?page=";
+let count = 1;
+
+function createBtn(className) {
+    const btn = document.createElement("button");
+    btn.className = className;
+    btn.innerText = "Get data";
+    return btn;
+}
+
+function createList(className) {
+    const list = document.createElement("ul");
+    list.className = className;
+    return list;
+}
+
+const getDataBtn = createBtn("btn");
+getDataBtn.addEventListener("click", (event) => {
+    event.target.innerText = "getting data...";
+    event.target.setAttribute("disabled", "disabled");
+    getData("GET", url + count++, onLoad, onError);
+})
+
+document.body.appendChild(createList("list"));
+document.body.appendChild(getDataBtn);
